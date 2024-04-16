d0274ce52c7790b86c949f3dcde359cbb9645316
chinkk0803 <161556165+chinkk0803@users.noreply.github.com>
2024-04-16 15:47:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-2.cl2oa0yseydp.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "chinkinkam-employee"
+customregion = "us-east-1"
+
